df4f2fb7a748022dd64fa7fc6914f9796188656c
 İsmet Ataman <atamanismet98@gmail.com>
2022-09-23 23:03:11 +0300

    Updated

--- Stacky Dash Clone/Assets/Scripts/StackController.cs
+++ Stacky Dash Clone/Assets/Scripts/StackController.cs
@@ -7,8 +7,11 @@ public class StackController : MonoBehaviour
 {
     public UIManager uimanager;
     public Vector3 stackPos;
+    private AudioSource audioSource;
+    public AudioClip collectClip;
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
         stackPos = Vector3.zero;
     }
   
@@ -16,11 +19,12 @@ public class StackController : MonoBehaviour
     {
         if(other.gameObject.CompareTag("stack"))
         {
+            audioSource.PlayOneShot(collectClip,1f);
             stackPos += new Vector3(0,0.9f,0);
             Stacklist.instance.stack.Add(other.gameObject);
             other.gameObject.GetComponent<BoxCollider>().enabled = false;
             other.gameObject.transform.parent = GameObject.Find("Stack").transform;
-            other.transform.DOLocalMove(stackPos,0.2f);
+            other.transform.DOLocalMove(stackPos,0f);
             this.gameObject.transform.GetChild(1).transform.localPosition += new Vector3(0,0.9f,0);
             Debug.Log("Listeye eklendi");
 
